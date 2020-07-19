ce32e6a27f0f4d6dc021c3397fc0fe06657f9344
Gabriel Perez <gabrielperez@Gabriels-MBP.iptv.microsoft.com>
2020-07-18 23:55:02 -0400

    add animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,36 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Lottie from "react-lottie"
+import animationData from "../assets/lotties/education.json"
+
+import { motion } from "framer-motion"
+
+const defaultOptions = {
+  autoplay: true,
+  animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid",
+  },
+}
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>{" "}
+    <Lottie options={defaultOptions} height={400} width={400} />
+    <motion.div
+      animate={{
+        scale: [1, 2, 2, 1, 1],
+        rotate: [0, 0, 270, 270, 0],
+        backgroundColor: "#000",
+        borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+      }}
+    >
+      <p>sada</p>
+    </motion.div>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
