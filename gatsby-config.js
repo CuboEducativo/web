cce962ff12e56fb2611cfc43c033f8b28d6314f2
Gabriel Pérez <gabriel@garox.org>
2021-06-18 21:47:34 -0400

    remove

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,44 +55,44 @@ module.exports = {
         display: "swap",
       },
     },
-    {
-      resolve: `gatsby-source-wordpress-experimental`,
-      options: {
-        url: process.env.WPGRAPHQL_URL,
-        verbose: true,
-        develop: {
-          hardCacheMediaFiles: true,
-        },
-        debug: {
-          graphql: {
-            writeQueriesToDisk: true,
-          },
-        },
-        html: {
-          fallbackImageMaxWidth: 800,
-        },
-        // fields can be excluded globally.
-        // this example is for wp-graphql-gutenberg.
-        // since we can get block data on the `block` field
-        // we don't need these fields
-        excludeFieldNames: [`blocksJSON`, `saveContent`],
-        type: {
-          Post: {
-            limit:
-              process.env.NODE_ENV === `development`
-                ? // Lets just pull 50 posts in development to make it easy on ourselves.
-                  35
-                : // And then we can pull all posts in production
-                  null,
-          },
-          // this shows how to exclude entire types from the schema
-          // this example is for wp-graphql-gutenberg
-          CoreParagraphBlockAttributesV2: {
-            exclude: true,
-          },
-        },
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-wordpress-experimental`,
+    //   options: {
+    //     url: process.env.WPGRAPHQL_URL,
+    //     verbose: true,
+    //     develop: {
+    //       hardCacheMediaFiles: true,
+    //     },
+    //     debug: {
+    //       graphql: {
+    //         writeQueriesToDisk: true,
+    //       },
+    //     },
+    //     html: {
+    //       fallbackImageMaxWidth: 800,
+    //     },
+    //     // fields can be excluded globally.
+    //     // this example is for wp-graphql-gutenberg.
+    //     // since we can get block data on the `block` field
+    //     // we don't need these fields
+    //     excludeFieldNames: [`blocksJSON`, `saveContent`],
+    //     type: {
+    //       Post: {
+    //         limit:
+    //           process.env.NODE_ENV === `development`
+    //             ? // Lets just pull 50 posts in development to make it easy on ourselves.
+    //               35
+    //             : // And then we can pull all posts in production
+    //               null,
+    //       },
+    //       // this shows how to exclude entire types from the schema
+    //       // this example is for wp-graphql-gutenberg
+    //       CoreParagraphBlockAttributesV2: {
+    //         exclude: true,
+    //       },
+    //     },
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
